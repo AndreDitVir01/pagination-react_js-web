680e225f98955d2ac60b401d76bcdd72999641f5
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-20 15:06:20 +0700

    ONG: Pagination and install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+import Posts from './components/Posts';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currectPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fecthPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('xxx.com');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  // Ambil post sekarang
+  const indexOfLastPost = currectPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currectPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currectPosts} loading={loading} />
     </div>
   );
 }
