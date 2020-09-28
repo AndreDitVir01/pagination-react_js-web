0162ef5c7dc6c68de63f2f9b2f497470ea867034
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-28 07:42:34 +0700

    Updates

---
+++ precache-manifest.43132f9829e15322fb70a051518956ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230f2b82a0769c433de3b6e7cd3a8fae",
+    "url": "/pagination-react_js-web/index.html"
+  },
+  {
+    "revision": "dee201701cf3b852a299",
+    "url": "/pagination-react_js-web/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "305b0eb762f482be407f",
+    "url": "/pagination-react_js-web/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "dee201701cf3b852a299",
+    "url": "/pagination-react_js-web/static/js/2.fcd0c435.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pagination-react_js-web/static/js/2.fcd0c435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305b0eb762f482be407f",
+    "url": "/pagination-react_js-web/static/js/main.db9b5227.chunk.js"
+  },
+  {
+    "revision": "5ed55e8b62053047ca3c",
+    "url": "/pagination-react_js-web/static/js/runtime-main.06c55681.js"
+  }
+]);
